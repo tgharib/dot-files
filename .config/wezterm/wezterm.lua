2531c98efa1b143c3ee1ca65d446c1c51b396b6e
Taha <tgharib@users.noreply.github.com>
2022-07-18 18:49:12 -0400

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,40 @@
+local wezterm = require 'wezterm';
+local act = wezterm.action
+
+return {
+  font = wezterm.font("Iosevka Term", {weight="Medium", stretch="Expanded"}),
+  font_size = 13,
+  font_dirs = {"fonts"},
+  font_locator = "ConfigDirsOnly",
+  color_scheme = "Solarized Dark Higher Contrast",
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  enable_tab_bar = false,
+  keys = {
+    {key="U", mods="CTRL", action=act.ScrollByPage(-0.5)},
+    {key="D", mods="CTRL", action=act.ScrollByPage(0.5)},
+    {key="K", mods="CTRL", action=act.ScrollByLine(-1)},
+    {key="J", mods="CTRL", action=act.ScrollByLine(1)},
+    {key="E", mods="CTRL",
+     action=wezterm.action.QuickSelectArgs{
+         label = "open url",
+         patterns={
+            "https?://\\S+"
+         },
+         action = wezterm.action_callback(function(window, pane)
+            local url = window:get_selection_text_for_pane(pane)
+            wezterm.log_info("opening: " .. url)
+            wezterm.open_with(url)
+         end)
+       }
+     },
+  },
+}
+
+-- Scrollback mode: Control+Shift+F
+-- Quick select mode: Control+Shift+Space
+-- Copy mode: Control+Shift+X
