ce2b3e9e1d335f6f1897870ed63856b25af76aa6
Taha Gharib <tgharib@users.noreply.github.com>
2022-10-07 15:12:56 -0400

    Update setup-linux.sh

--- .manual/setup-linux.sh
+++ .manual/setup-linux.sh
@@ -6,9 +6,9 @@ wget https://github.com/DavHau/nix-portable/releases/download/v009/nix-portable
 chmod +x nix-portable
 ln -s ./nix-portable ./nix
 
-cat <<EOF > ~/.bashrc
+cat << 'EOF' > ~/.bashrc
 
-PATH="\${PATH:+\${PATH}:}~/bin"
+PATH="${PATH:+${PATH}:}~/bin"
 alias enter-dev='NP_RUNTIME=bwrap nix shell github:nixos/nixpkgs/nixos-22.05#{ripgrep,sd,fd,fzf,abduco,lazygit,du-dust,bat,btop,libqalculate,clifm,neovim,tree-sitter,nodejs}'
 # github:nixos/nixpkgs/nixos-unstable
 alias machine-clean='~/bin/nix-portable nix-collect-garbage -d'
