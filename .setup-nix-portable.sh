f6f2bb258066c19ae8ebad09aa893b8ba9abd181
Taha Gharib <6457015+tgharib@users.noreply.github.com>
2024-08-01 15:34:17 -0700

    Update .setup-nix-portable.sh

--- .setup-nix-portable.sh
+++ .setup-nix-portable.sh
@@ -11,7 +11,7 @@ cat << 'EOF' >> ~/.bashrc
 
 # Nix-portable
 export PATH="${PATH:+${PATH}:}~/bin"
-alias enter-dev='nix-portable nix shell github:nixos/nixpkgs/nixos-24.05#{ripgrep,sd,fd,as-tree,fzf,lazygit,du-dust,bat,btop,difftastic,hexyl,pazi,minicom,yazi,neovim,tree-sitter,nodejs}'
+alias enter-dev='nix-portable nix shell github:nixos/nixpkgs/nixos-24.05#{ripgrep,sd,fd,as-tree,fzf,lazygit,du-dust,bat,btop,hexyl,pazi,minicom,yazi,neovim,tree-sitter,nodejs}'
 alias machine-clean='nix-portable nix-collect-garbage -d'
 
 # Source bashrc files
@@ -20,14 +20,4 @@ source ~/.bashrc-nonix
 if [[ $NIX_PATH ]]; then
   source ~/.bashrc-dev
 fi
-EOF
-
-# USE DIFFTASTIC
-cat << 'EOF' >> ~/.gitconfig
-
-[core]
-  pager = "less -XF"
-
-[diff]
-  external = "difft --color auto --background dark --display inline"
 EOF
