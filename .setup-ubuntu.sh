7c99494a02ea62a085a1a905cac453772c18efdd
Taha <6457015+tgharib@users.noreply.github.com>
2024-09-06 15:27:37 -0700

    Update

--- .setup-ubuntu.sh
+++ .setup-ubuntu.sh
@@ -15,7 +15,7 @@ source ~/.bashrc-nonix
 source ~/.bashrc-dev
 EOF
 
-run sudo apt install kitty dex fd-find ripgrep fzf sd wl-clipboard sway build-essential btop i3status wlr-randr pulsemixer gammastep swayidle lxqt-policykit remind buku syncthing neovim
+run sudo apt install kitty dex fd-find ripgrep fzf sd wl-clipboard sway build-essential btop i3status wlr-randr pulsemixer gammastep swayidle lxqt-policykit remind buku syncthing neovim keepassxc
 # mako, pazi
 run sudo ln -s /usr/bin/fdfind /usr/bin/fd
 
