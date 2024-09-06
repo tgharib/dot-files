42b6a224895d9c9585c277ae296bb5bce1f73dfe
Taha <6457015+tgharib@users.noreply.github.com>
2024-09-06 15:17:09 -0700

    Update

--- .setup-ubuntu.sh
+++ .setup-ubuntu.sh
@@ -1,9 +1,45 @@
 #!/usr/bin/env bash
 
-# ADD TO BASHRC
-cat << 'EOF' >> ~/.bashrc
+run() {
+  printf '%s ' "run $@?" > /dev/tty
+  IFS= read -r answer < /dev/tty
+  case $answer in
+    [yY]*) eval "$@";;
+  esac
+}
+
+run cat << 'EOF' >> ~/.bashrc
 
 # Source bashrc files
 source ~/.bashrc-nonix
 source ~/.bashrc-dev
 EOF
+
+run sudo apt install kitty dex fd-find ripgrep fzf sd wl-clipboard sway build-essential btop i3status wlr-randr pulsemixer gammastep swayidle lxqt-policykit remind buku syncthing
+# mako, pazi
+run sudo ln -s /usr/bin/fdfind /usr/bin/fd
+
+run cd ~/Downloads/
+run wget https://github.com/be5invis/Iosevka/releases/download/v31.5.0/SuperTTC-Iosevka-31.5.0.zip
+run unzip SuperTTC-Iosevka-31.5.0.zip
+run mkdir ~/.fonts/
+run mv Iosevka.ttc ~/.fonts/
+
+# Laptop only below
+
+run mkdir /etc/keyd/
+run tee /etc/keyd/default.conf <<EOF
+[ids]
+*
+
+[main]
+# Maps capslock to escape when pressed and meta when held.
+capslock = overload(meta, esc)
+
+# Remaps the escape key to capslock
+# esc = capslock
+EOF
+
+run sudo add-apt-repository ppa:keyd-team/ppa
+run sudo apt update
+run sudo apt install keyd
