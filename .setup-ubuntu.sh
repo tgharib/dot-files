7d88ef28c5e982512076b27211a38f444591867d
Taha <6457015+tgharib@users.noreply.github.com>
2024-09-06 21:24:34 -0700

    Update

--- .setup-ubuntu.sh
+++ .setup-ubuntu.sh
@@ -15,10 +15,11 @@ source ~/.bashrc-nonix
 source ~/.bashrc-dev
 EOF
 
-run sudo apt install kitty dex fd-find ripgrep fzf sd wl-clipboard sway build-essential btop i3status wlr-randr pulsemixer gammastep swayidle lxqt-policykit remind buku syncthing neovim keepassxc light
+run sudo apt install kitty dex fd-find ripgrep fzf sd wl-clipboard sway build-essential btop i3status wlr-randr pulsemixer gammastep swayidle lxqt-policykit remind buku syncthing neovim keepassxc light curl bat
 # mako, pazi
 run sudo adduser owner video
 run sudo ln -s /usr/bin/fdfind /usr/bin/fd
+run sudo ln -s /usr/bin/batcat /usr/bin/bat
 
 run cd ~/Downloads/
 run wget https://github.com/be5invis/Iosevka/releases/download/v31.5.0/SuperTTC-Iosevka-31.5.0.zip
@@ -49,3 +50,11 @@ EOF
 run sudo add-apt-repository ppa:keyd-team/ppa
 run sudo apt update
 run sudo apt install keyd
+
+sudo add-apt-repository ppa:linrunner/tlp
+sudo apt update
+sudo apt install tlp tlp-rdw powertop
+sudo systemctl enable tlp.service
+# disable bluetooth and enable battery cary in /etc/tlp.conf
+# add acpi.ec_no_wakeup=1 kernel parameter
+# sudo powertop to check wattage
