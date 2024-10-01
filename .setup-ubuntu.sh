c897f369eca9fcb1e961e48a5c4b6c8a9484ba56
Taha <6457015+tgharib@users.noreply.github.com>
2024-10-01 08:46:06 -0700

    Add screenshot tools

--- .setup-ubuntu.sh
+++ .setup-ubuntu.sh
@@ -15,7 +15,7 @@ source ~/.bashrc-nonix
 source ~/.bashrc-dev
 EOF
 
-run sudo apt install kitty dex fd-find ripgrep fzf sd wl-clipboard sway build-essential btop i3status wlr-randr pulsemixer gammastep swayidle lxqt-policykit remind buku syncthing neovim keepassxc light curl bat
+run sudo apt install kitty dex fd-find ripgrep fzf sd wl-clipboard sway build-essential btop i3status wlr-randr pulsemixer gammastep swayidle lxqt-policykit remind buku syncthing neovim keepassxc light curl bat slurp grim
 # mako, pazi
 run sudo adduser owner video
 run sudo ln -s /usr/bin/fdfind /usr/bin/fd
