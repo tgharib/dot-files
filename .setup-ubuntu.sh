0e391506380a7294e92836cefbfd6eedbb2c6a74
Taha <6457015+tgharib@users.noreply.github.com>
2024-11-30 13:23:41 -0800

    Fix typo

--- .setup-ubuntu.sh
+++ .setup-ubuntu.sh
@@ -37,7 +37,7 @@ run rm NerdFontsSymbolsOnly.tar.xz LICENSE README.md 10-nerd-font-symbols.conf
 run mv SymbolsNerdFontMono-Regular.ttf ~/.fonts/
 run mv SymbolsNerdFont-Regular.ttf ~/.fonts/
 
-run LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*') && curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" && tar xf lazygit.tar.gz lazygit && sudo install lazygit -D -t /usr/local/bin/ && rm lazygit && rm lazgit.tar.gz
+run LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*') && curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" && tar xf lazygit.tar.gz lazygit && sudo install lazygit -D -t /usr/local/bin/ && rm lazygit && rm lazygit.tar.gz
 
 # Setup firefox by logging into firefox account
 
