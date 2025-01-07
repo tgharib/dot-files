98fa799df22b78653f1f65d38708b83a0cb477dc
Taha Gharib <taha.gharib@sambanovasystems.com>
2025-01-07 15:02:49 -0800

    Fix

--- .setup-portable.sh
+++ .setup-portable.sh
@@ -4,7 +4,7 @@
 cat << 'EOF' >> ~/.bashrc
 
 export PATH="${PATH:+${PATH}:}~/bin:~/.cargo/bin:~/bin/nvim/usr/bin"
-# install rustup for cargo, rg, sd, fd, fzf, lazygit, dust, as-tree, btop, pazi, nvim, tree-sitter, nodejs
+# install rustup for cargo, rg, sd, fd, fzf, lazygit, dust, as-tree, btop, pazi, nvim, nodejs
 
 # Source bashrc files
 source ~/.bashrc-portable
