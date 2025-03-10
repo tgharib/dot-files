8cfb3f1784e15fb93bd49ccec7d27041f8d43a98
Taha Gharib <taha.gharib@sambanovasystems.com>
2025-03-10 21:48:57 +0000

    Update .setup-portable.sh

--- .setup-portable.sh
+++ .setup-portable.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
-# ADD TO BASHRC
-cat << 'EOF' >> ~/.bashrc
+run() {
+  printf '%s ' "run $@?" > /dev/tty
+  IFS= read -r answer < /dev/tty
+  case $answer in
+    [yY]*) eval "$@";;
+  esac
+}
 
-export PATH="${PATH:+${PATH}:}~/bin:~/.cargo/bin:~/bin/nvim/bin"
-# install rustup for cargo, rg, sd, fd, fzf, lazygit, dust, as-tree, btop, pazi, nvim, nodejs
+# Add to bashrc
+run cat << 'EOF' >> ~/.bashrc
 
-# Source bashrc files
+export PATH="${PATH:+${PATH}:}~/bin:~/.cargo/bin:~/bin/nvim-linux-x86_64/bin"
 source ~/.bashrc-portable
 EOF
+
+# Install rustup for cargo, rg, sd, fd, fzf, lazygit, dust, as-tree, btop, pazi, nodejs
+
+# Install nvim
+run mkdir ~/bin
+run cd ~/bin
+run wget https://github.com/neovim/neovim-releases/releases/download/v0.10.4/nvim-linux-x86_64.tar.gz
+run tar xvf nvim-linux-x86_64.tar.gz
+run rm nvim-linux-x86_64.tar.gz
