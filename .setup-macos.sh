2bc50aac3948fbca01fb2435c19364df78482812
Taha <6457015+tgharib@users.noreply.github.com>
2025-09-16 14:48:48 -0400

    Update setup-macos.sh

--- .setup-macos.sh
+++ .setup-macos.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 # Install homebrew (macos user needs to be an admin)
-# Install Karabiner, f.lux, VS Code, mpv, obs, neovim
+# Install Karabiner, f.lux, VS Code, mpv, obs, neovim, lazygit
 # Optional: AltTab, Smooze Pro
 # Follow .setup-ubuntu.sh to install Iosevka.ttc and SymbolsNerdFontMono-Regular.ttf and SymbolsNerdFont-Regular.ttf
