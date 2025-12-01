a5723db5c2d5118fe6c40be2371a441872d03e3b
Taha <6457015+tgharib@users.noreply.github.com>
2025-12-01 09:05:34 -0500

    Update macos config

--- .setup-macos.sh
+++ .setup-macos.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env zsh
 
 # Install homebrew (macos user needs to be an admin)
-# Install Karabiner, f.lux, VS Code, mpv, obs, neovim, lazygit
+brew install karabiner flux mpv obs neovim lazygit btop tmuxinator
 # Optional: AltTab, Smooze Pro
+brew install --cask --no-quarantine middleclick
+
 # Follow .setup-ubuntu.sh to install Iosevka.ttc and SymbolsNerdFontMono-Regular.ttf and SymbolsNerdFont-Regular.ttf
 
 # https://stackoverflow.com/questions/77052638/changing-default-shell-from-zsh-to-bash-on-macos-catalina-and-beyond
 # Changing shell to bash requires a restart
-# Add `source ~/.bashrc-portable` to ~/.bash_profile
-
-brew install --cask --no-quarantine middleclick
+# Add `source ~/.bashrc-portable and PATH export` to ~/.bash_profile
 
 # https://www.npmjs.com/package/gitsm
 npm install -g gitsm
